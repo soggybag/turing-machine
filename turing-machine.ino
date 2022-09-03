636d2477182781d7e5d692f7dedc6ac568c913c3
Mitchell Hudson <soggybag@gmail.com>
2022-09-02 20:52:43 -0700

    added timer

--- turing-machine.ino
+++ turing-machine.ino
@@ -1,19 +1,31 @@
 // Mock up of Music Thing Modular Turing Machine with Arduino
+// Added MsTimer, moved shiftregister code out of loop
 
+
+// Includes -------------------------------------------------
+#include <MsTimer2.h>
+
+
+// Declare vars ---------------------------------------------
 int ledPin[] = {6, 7, 8, 9, 10, 11, 12, 13};
+int cv_out_pin = 5;
 
-byte d = 0b00101101; // starting binary value
+byte d = 0b10001101; // starting binary value
 
 int pot_1 = 0; // value read from pot 1 A0 clock speed
 int pot_2 = 0; // value read from pot 2 A1 random probablity
 
-// Init
+unsigned long count = 400;
+unsigned long delay_ms = 0;
+
+
+// Setup ----------------------------------------------------
 void setup() {
   // Random
   randomSeed(millis());
 
   // Init d
-  d = random(0, 255);
+//  d = random(0, 255);
   
   // bytes
   for (int i = 0; i < 8; i++) {
@@ -21,39 +33,62 @@ void setup() {
   }
 
   // value 
-  pinMode(3, OUTPUT);
+  pinMode(cv_out_pin, OUTPUT);
+  analogWrite(cv_out_pin, d);
   
   // Testing
   Serial.begin(9600);
+
+  // setup timer
+  
+  MsTimer2::set(1, setCount);
+  MsTimer2::start();
 }
 
-// main
+// main ----------------------------------------------------
 void loop() {
   // Read Pots
   readPots();
-  long delay_ms = map(pot_1, 0, 1023, 0, 1000);
+  delay_ms = map(pot_1, 0, 1023, 0, 1000);
+
+  if (count > delay_ms) {
+//    Serial.print("pot_1:");
+//    Serial.print(pot_1);
+//    Serial.print(" delay ms:");
+//    Serial.print(delay_ms);
+//    Serial.println();   
+
+    long r = random(0, 1000);
+    
+//    Serial.print("pot_2: ");
+//    Serial.print(pot_2);
+//    Serial.print(" r:");
+//    Serial.println(r);
+    
+    count = 0;
+
+    //  rotate
+    d = rotateByte(d, 1);
+    
+    // flip last bit maybe
+    if (r < pot_2) {
+      d = (d ^ (1 << 7));
+    }
   
-  //  rotate
-  d = rotateByte(d, 1);
-
-  // flip last bit maybe
-  if (random(0, 1000) < pot_2) {
-    d = (d ^ (1 << 7));
-    Serial.println(int(d));
+    // Display byte
+    displayBinary(d);
+    
+    // Display value
+    Serial.print("D:");
+    Serial.println(d);
+    
+    analogWrite(cv_out_pin, d);
   }
+}
 
-  // Display byte
-  displayBinary(d);
-
-  // Display value
-  analogWrite(3, d);
-
-  delay(delay_ms);
-
-  // Testing 
-  // Serial.print("byte int: ");
-  // Serial.println(int(d));
-  // Serial.println(pot_2);
+// Set Count -----------------------------------------------
+void setCount() {
+  count ++;
 }
 
 // rotate byte
